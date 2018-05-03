05bf9a6dfbe1ec145a88660353c79765ea8e8d75
lkxx <31329095+lkxx@users.noreply.github.com>
2018-05-03 10:44:27 +0800

    Update daytime.py

--- daytime.py
+++ daytime.py
@@ -1,13 +1,23 @@
 # -*- coding:utf-8 -*-
 import json
 import csv
-import datetime  
-start='2016-06-01'  
-end='2017-01-01'  
+import datetime
+
+def dt1():
+    start='2016-06-01'  
+    end='2016-07-01'  
+       
+    datestart=datetime.datetime.strptime(start,'%Y-%m-%d')  
+    dateend=datetime.datetime.strptime(end,'%Y-%m-%d')  
+       
+    while datestart<dateend:  
+        datestart+=datetime.timedelta(days=1)  
+        print datestart.strftime('%Y-%m-%d')  
+
    
-datestart=datetime.datetime.strptime(start,'%Y-%m-%d')  
-dateend=datetime.datetime.strptime(end,'%Y-%m-%d')  
-   
-while datestart<dateend:  
-    datestart+=datetime.timedelta(days=1)  
-    print datestart.strftime('%Y-%m-%d')  
+def dt():  
+    begin = datetime.date(2014,6,1)  
+    end = datetime.date(2014,7,7)  
+    for i in range((end - begin).days+1):  
+        day = begin + datetime.timedelta(days=i)  
+        print str(day)  
